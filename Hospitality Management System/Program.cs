3bd49cc82713df78964758f0ac900ddd5888c3a4
Nikhil Sarwara <117506262+Nikhil-Sarwara@users.noreply.github.com>
2025-03-17 15:17:38 +1100

    add swagger for development testing

--- Hospitality Management System/Program.cs
+++ Hospitality Management System/Program.cs
@@ -1,7 +1,11 @@
 using HospitalityManagementSystem.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using MongoDB.Bson;
 using MongoDB.Driver;
+using Swashbuckle.AspNetCore;
 using System.Text;
 
 namespace Hospitality_Management_System;
@@ -16,6 +20,20 @@ public class Program
         var mongoClient = new MongoClient(builder.Configuration["MongoDB:ConnectionString"]);
         var database = mongoClient.GetDatabase(builder.Configuration["MongoDB:DatabaseName"]);
         builder.Services.AddSingleton(database);
+
+        // Test database connection
+        try
+        {
+            var collection = database.GetCollection<BsonDocument>("test");
+            collection.InsertOne(new BsonDocument());
+            Console.WriteLine("Database connection successful");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Database connection failed");
+            Console.WriteLine(ex.Message);
+            Environment.Exit(1);
+        }
 
         // Add User Service
         builder.Services.AddScoped<UserService>();
@@ -39,10 +57,49 @@ public class Program
         builder.Services.AddAuthorization();
         builder.Services.AddControllers();
 
+        // Add Swagger services
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo { Title = "Hospitality Management System API", Version = "v1" });
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter JWT with Bearer into field",
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey
+            });
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new[] { "Bearer" }
+        }
+            });
+        });
+
         var app = builder.Build();
+
+        // Add Swagger middleware
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "Hospitality Management System API V1");
+        });
 
         app.UseAuthentication();
         app.UseAuthorization();
+
+        app.UseCors(builder =>
+        builder.AllowAnyOrigin()
+           .AllowAnyMethod()
+           .AllowAnyHeader());
 
         app.UseHttpsRedirection();
         app.UseStaticFiles();
@@ -53,3 +110,4 @@ public class Program
         app.Run();
     }
 }
+
