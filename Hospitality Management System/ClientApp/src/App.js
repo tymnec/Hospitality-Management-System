1cf9db8b694e14c32a1fb1a48e498aae00db9d6b
Nikhil Sarwara <117506262+Nikhil-Sarwara@users.noreply.github.com>
2025-03-17 16:09:40 +1100

    add user login in frontend

--- Hospitality Management System/ClientApp/src/App.js
+++ Hospitality Management System/ClientApp/src/App.js
@@ -1,8 +1,15 @@
-import React, { Component } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import AppRoutes from './AppRoutes';
-import { Layout } from './components/Layout';
-import './custom.css';
+import React, { Component } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import AppRoutes from "./AppRoutes";
+import { Layout } from "./components/Layout";
+import Login from "./components/Login";
+import "./custom.css";
+import { Home } from "./components/Home";
+
+// PrivateRoute Component
+const PrivateRoute = ({ children }) => {
+  return localStorage.getItem("token") ? children : <Navigate to="/login" />;
+};
 
 export default class App extends Component {
   static displayName = App.name;
@@ -11,6 +18,20 @@ export default class App extends Component {
     return (
       <Layout>
         <Routes>
+          {/* You can manually define routes for login and dashboard if needed */}
+          <Route path="/login" element={<Login />} />
+
+          {/* Private route for dashboard */}
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <Home />
+              </PrivateRoute>
+            }
+          />
+
+          {/* Add additional dynamic routes using AppRoutes */}
           {AppRoutes.map((route, index) => {
             const { element, ...rest } = route;
             return <Route key={index} {...rest} element={element} />;
