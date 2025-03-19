d2e2bacfa60bda2df24a55bcabb1bd820974043a
Nikhil Sarwara <117506262+Nikhil-Sarwara@users.noreply.github.com>
2025-03-20 02:45:27 +1100

    add feature of check authentication in user settings page

--- Hospitality Management System/ClientApp/src/components/Settings/SettingsUseEffect.js
+++ Hospitality Management System/ClientApp/src/components/Settings/SettingsUseEffect.js
@@ -1,8 +1,14 @@
 import axios from "axios";
 
 export const fetchUserData = async (setFormData) => {
+  const token = localStorage.getItem("token");
+  if (!token || token === "undefined") {
+    alert("You need to login first!");
+    window.location.href = "/login";
+    return;
+  }
+
   try {
-    const token = localStorage.getItem("token");
     const response = await axios.get("api/auth/settings", {
       headers: {
         Authorization: `Bearer ${token}`,
