28b9058dd65130a711b8f9a5a393b0c7f8829a4c
Nikhil Sarwara <117506262+Nikhil-Sarwara@users.noreply.github.com>
2025-03-28 21:14:53 +1100

    fix: index

--- Hospitality Management System/ClientApp/src/index.js
+++ Hospitality Management System/ClientApp/src/index.js
@@ -1,20 +1,15 @@
 import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
+import AppRoutes from "./AppRoutes";
 
-const baseUrl = document.getElementsByTagName("base")[0].getAttribute("href");
+// const baseUrl = document.getElementsByTagName("base")[0].getAttribute("href");
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(
-  <BrowserRouter basename={baseUrl}>
-    <App />
-  </BrowserRouter>
-);
+root.render(<AppRoutes />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
